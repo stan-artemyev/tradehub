66157300070f4f8983a53fe91bef26e7aa57d67b
Stanislav Artemyev <stan.artemyev.ca@gmail.com>
2024-06-12 16:43:23 -0700

    update sell.js and tradeForm.js with "

--- static/sell.js
+++ static/sell.js
@@ -1,12 +1,13 @@
-// Updates number of available shares based on the user's selection
+// Updates number of available shares based on the user"s selection
 
-document.getElementById('symbolSelect').addEventListener('change', function() {
+document.getElementById("symbolSelect").addEventListener("change", function() {
     var selectedOption = this.selectedOptions[0];
-    var availableShares = selectedOption.getAttribute('data-available-shares');
-    document.getElementById('available_shares').innerText = availableShares || '';
+    var availableShares = selectedOption.getAttribute("data-available-shares");
+    document.getElementById("available_shares").innerText = availableShares || "";
+    document.getElementById("available_shares_container").hidden = false; // Make the div visible
 });
 
-// Ensure the available shares are initially set to an empty string on page load
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('available_shares').innerText = '';
+// Ensure the available shares are initially set to an empty string on page load/re-load
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("available_shares").innerText = "";
 });
--- static/tradeForm.js
+++ static/tradeForm.js
@@ -1,12 +1,12 @@
 // Function that sets action in a tradeForm based on the Buy/Sell button clicked and submits the form
 function setAction(action) {
-    var tradeForm = document.getElementById('tradeForm');
+    var tradeForm = document.getElementById("tradeForm");
 
     // Modify the form action based on the action to set proper route for backend
-    if (action === 'buy') {
-        tradeForm.action = '/buy'; // Change the action to '/buy' for Buy button
-    } else if (action === 'sell') {
-        tradeForm.action = '/sell'; // Change the action to '/sell' for Sell button
+    if (action === "buy") {
+        tradeForm.action = "/buy"; // Change the action to "/buy" for Buy button
+    } else if (action === "sell") {
+        tradeForm.action = "/sell"; // Change the action to "/sell" for Sell button
     }
 
     // Submit the form
